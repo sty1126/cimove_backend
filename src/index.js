229cd04a3085f6108a452a9d192f93196a207963
sty1126 <sarah26abril@gmail.com>
2025-03-06 01:22:17 -0500

    put, create y get de inventario (general)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { PORT } from "./config.js";
 import productosRoutes from "./routes/productos.routes.js";
+import inventarioRoutes from "./routes/inventario.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(productosRoutes);
+app.use(inventarioRoutes);
 
 // Asignación de puerto importada desde config.js
 app.listen(PORT);
