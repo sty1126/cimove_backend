d995aa1258cb39ceec5042f8e5cbc412af0b47c9
Marioo440 <94205237+Marioo440@users.noreply.github.com>
2025-03-12 22:54:56 -0500

    Create movimiento.controller.js
    controller del movimiento de producto

---
+++ src/controllers/movimiento.controller.js
@@ -0,0 +1,38 @@
+import { pool } from "../db.js";
+
+// Obtener todos los movimientos de productos
+export const getMovimientos = async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT ID_MOV, ID_TIPOMOV, ID_PRODUCTO, CANTIDAD_MOV, FECHA_MOVIMIENTO, ESTADO_MOVIMIENTO FROM MOVPRODUCTO"
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error al obtener movimientos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
+
+// Agregar un nuevo movimiento de producto
+export const createMovimiento = async (req, res) => {
+  try {
+    const { id_tipomov, id_producto, cantidad_mov, fecha_movimiento, estado_movimiento } = req.body;
+
+    
+    if (!id_tipomov || !id_producto || !cantidad_mov || !fecha_movimiento || !estado_movimiento) {
+      return res.status(400).json({ error: "Todos los campos son obligatorios" });
+    }
+
+    // Insertar el nuevo movimiento de producto
+    const result = await pool.query(
+      "INSERT INTO MOVPRODUCTO (ID_TIPOMOV, ID_PRODUCTO, CANTIDAD_MOV, FECHA_MOVIMIENTO, ESTADO_MOVIMIENTO) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [id_tipomov, id_producto, cantidad_mov, fecha_movimiento, estado_movimiento]
+    );
+
+    // Devolver el movimiento creado recientemente
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error("Error al agregar movimiento:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
