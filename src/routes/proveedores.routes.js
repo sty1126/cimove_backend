59c7092c19dd1ba65de9520df6ba1805f752c942
Marioo440 <94205237+Marioo440@users.noreply.github.com>
2025-03-29 11:19:36 -0500

    Update proveedores.routes.js
    Se actualizan rutas de proveedor

--- src/routes/proveedores.routes.js
+++ src/routes/proveedores.routes.js
@@ -6,18 +6,20 @@ import {
   createProveedor,
   updateProveedor,
   deleteProveedor,
+  createTipoProveedor
 } from "../controllers/proveedores.controllers.js";
 
 const router = Router();
 
-// Rutas de tipos de proveedores
-router.get("/tipos", getTiposProveedores); // Obtener todos los tipos de proveedores
+router.get("/tipos", getTiposProveedores); 
 
-// Rutas de proveedores
-router.get("/all", getProveedores); // Obtener todos los proveedores
-router.get("/:id", getProveedorById); // Obtener un proveedor por ID
-router.post("/", createProveedor); // Crear un nuevo proveedor
-router.put("/:id", updateProveedor); // Actualizar un proveedor
-router.put("/eliminar/:id", deleteProveedor); // Eliminar un proveedor
+
+router.get("/all", getProveedores); 
+router.get("/:id", getProveedorById); 
+router.post("/", createProveedor); 
+router.put("/:id", updateProveedor); 
+router.put("/eliminar/:id", deleteProveedor); 
+router.get("/proveedores/tipos", getTiposProveedores);
+router.post("/proveedores/tipos", createTipoProveedor);
 
 export default router;
