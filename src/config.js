377d6469f10c2c4cff3f4579f843baccb23a08f4
Marioo440 <94205237+Marioo440@users.noreply.github.com>
2025-04-18 00:36:21 -0500

    Update config.js
    Semana santa

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const DB_USER = "postgres";
 export const DB_HOST = "localhost";
-export const DB_PASSWORD = "12345";
+export const DB_PASSWORD = "123321";
 export const DB_DATABASE = "CIMOVE";
 export const DB_PORT = 5432;
 
