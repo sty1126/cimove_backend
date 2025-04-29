84780a1889aad7b3a84c773ae126711dafc18e5f
sty1126 <sarah26abril@gmail.com>
2025-04-29 17:18:35 -0500

    Colapso4

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config(); // Cargar las variables del archivo .env
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_URL:", process.env.DB_URL);
+
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_HOST = process.env.DB_HOST;
