c71eae00b0fe437d07bab75afbd9106ad7fc3cba
sty1126 <sarah26abril@gmail.com>
2025-04-29 17:25:02 -0500

    Colapso6

--- src/config.js
+++ src/config.js
@@ -8,11 +8,12 @@ console.log("DB_DATABASE:", process.env.DB_DATABASE);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB_URL:", process.env.DB_URL);
 
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_PORT = process.env.DB_PORT;
-export const DB_URL = process.env.DB_URL;
+export const DB_USER = "user";
+export const DB_PASSWORD = "9BP95pUndVwsKckL8DZntyHwYPf11XfV";
+export const DB_HOST = "dpg-d08539vdiees738tpe3g-a.oregon-postgres.render.com";
+export const DB_DATABASE = "cimove";
+export const DB_PORT = "5432";
+export const DB_URL =
+  "postgres://user:9BP95pUndVwsKckL8DZntyHwYPf11XfV@dpg-d08539vdiees738tpe3g-a.oregon-postgres.render.com/cimove";
 
 export const PORT = process.env.PORT || 4000;
