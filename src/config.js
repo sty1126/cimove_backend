3be5252e5f6791e414348a041e693053e297319e
sty1126 <sarah26abril@gmail.com>
2025-04-29 16:45:01 -0500

    Bien

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const DB_USER = "user";
-export const DB_URL = "@dpg-d08539vdiees738tpe3g-a.oregon-postgres.render.com/cimove";
+export const DB_URL =
+  "@dpg-d08539vdiees738tpe3g-a.oregon-postgres.render.com/cimove";
 export const DB_PASSWORD = "9BP95pUndVwsKckL8DZntyHwYPf11XfV";
 export const DB_DATABASE = "cimove";
 export const DB_PORT = 5432;
