5cbed8a2a786df5d28df4e2c8615e70edb63d039
sty1126 <sarah26abril@gmail.com>
2025-08-28 00:33:10 -0500

    bd

--- src/config.js
+++ src/config.js
@@ -1,22 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// ================= LOCAL =================
-export const DB_USER = "user"; // usuario de Render para conexión externa
-export const DB_PASSWORD = "Z7z1NvSr9851YdrS3nv7mwomJ9vf6ydq";
-export const DB_HOST = "dpg-d23djfh5pdvs739rlceg-a.oregon-postgres.render.com";
-export const DB_DATABASE = "cimove_mofu";
+// ================= LOCAL (Conexión Externa) =================
+export const DB_USER = "user";
+export const DB_PASSWORD = "zOn1tEN8fVAbelj51aGHJKT0xxibJdCi";
+export const DB_HOST = "dpg-d2nka2be5dus73ftg920-a.oregon-postgres.render.com";
+export const DB_DATABASE = "cimove_db";
 export const DB_PORT = 5432;
 
 export const DB_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-// ================= PRODUCCIÓN (Render) =================
-// export const DB_URL = process.env.DATABASE_URL;
-// export const DB_USER = undefined;
-// export const DB_PASSWORD = undefined;
-// export const DB_HOST = undefined;
-// export const DB_DATABASE = undefined;
-// export const DB_PORT = undefined;
+// ================= PRODUCCIÓN (Render - Conexión Interna) =================
+// export const DB_URL = "postgresql://user:zOn1tEN8fVAbelj51aGHJKT0xxibJdCi@dpg-d2nka2be5dus73ftg920-a/cimove_db";
 
 export const PORT = process.env.PORT || 4000;
 
